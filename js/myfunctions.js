88dd34552dae84ed767a8d008cee68e578032f15
Lucas Liaño <lucas.liano2000@gmail.com>
2020-12-26 14:55:07 -0300

    Pre-Migrating Commit

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,3 +1,20 @@
+// En este archivo enumero las funciones propias.
+
+function createList()
+{
+  // Esta función crea una lista HTML con los archivos disponibles.
+  console.log("FALTA COMPLETAR createList()")
+  // Si o si hace falta de una backend
+  // const targetFolder = './pdf/';
+  // const fs = require('fs');
+  //
+  // fs.readdir(targetFolder, (err, files) => {
+  //   files.forEach(file => {
+  //     console.log(file);
+  //   });
+  // });
+}
+
 function modificarEstadoNavbar()
 {
   if(navbarResponsive.className == "navbar-collapse collapse show")
